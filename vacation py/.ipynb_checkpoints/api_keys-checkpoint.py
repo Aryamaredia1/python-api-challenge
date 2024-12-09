5d3c58f8eaa24e1b20e2a15fa1299b5a8201bd6c
Arya Maredia <arya.maredia1@gmail.com>
2024-12-09 17:17:27 -0600

    final

---
+++ vacation py/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "352424df86e720544a6e2eb7ecc66df7"
+
+# Geoapify API Key
+geoapify_key = "0ac36e4b4e5e447bbe66d194e165a00f"
+
---
+++ vacation py/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "352424df86e720544a6e2eb7ecc66df7"
+
+# Geoapify API Key
+geoapify_key = "b6c2dac572b7427a9024ebad6826daa7"
+
